ba79f37e358c87ff44238af678f8afa03d0f7940
Rustam Ibragimov <iamdaiver@gmail.com>
2019-03-28 11:48:03 +0300

    methods in migrations

--- lib/sequel/extensions/methods_in_migrations.rb
+++ lib/sequel/extensions/methods_in_migrations.rb
@@ -3,13 +3,16 @@
 require 'symbiont'
 
 Sequel::SimpleMigration.prepend(Module.new do
+
   def apply(db, direction)
-    unless [:up, :down].include?(direction)
+    # :nocov:
+    unless [:up, :down].include?(direction) # NOTE: original code
       raise(ArgumentError, "Invalid migration direction specified (#{direction.inspect})")
     end
+    # :nocov:
 
     if prok = public_send(direction)
-      Symbiont::Executor.evaluate(db, &prok)
+      Symbiont::Executor.evaluate(db, &prok) # NOTE: our extension
     end
   end
 end)
