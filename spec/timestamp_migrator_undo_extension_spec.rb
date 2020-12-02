22c68433aee9ac500b16c68dd192fb413263bdf5
Yuri Smirnov <tycooon@yandex.ru>
2020-12-02 17:47:08 +0300

    add spec

--- spec/timestamp_migrator_undo_extension_spec.rb
+++ spec/timestamp_migrator_undo_extension_spec.rb
@@ -14,4 +14,12 @@ RSpec.describe "timestamp_migrator_undo_extension" do
     create_migrator.undo(1549624163)
     expect(DB.tables).not_to include(:first_table)
   end
+
+  context "with missing migration" do
+    it "raises proper error" do
+      expect { create_migrator.undo(20201202130630) }.to raise_error(
+        Sequel::Migrator::Error, "Migration 20201202130630 does not exist in the filesystem"
+      )
+    end
+  end
 end
