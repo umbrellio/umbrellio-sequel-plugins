ce78f8d12807e3a6a09a8bada51226104f23860f
Yuri Smirnov <tycooon@yandex.ru>
2021-03-02 17:27:57 +0300

    fix rubocop issues

--- spec/extensions/pg_tools_spec.rb
+++ spec/extensions/pg_tools_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 DB.create_table :event_log
-DB.create_table :event_log_1, inherits: :event_log
-DB.create_table :event_log_2, inherits: :event_log
+DB.create_table :event_log1, inherits: :event_log
+DB.create_table :event_log2, inherits: :event_log
 
 RSpec.describe "pg_tools" do
   specify do
     tables = DB.inherited_tables_for(:event_log)
-    expect(tables).to eq([:event_log_1, :event_log_2])
+    expect(tables).to eq([:event_log1, :event_log2])
   end
 end
