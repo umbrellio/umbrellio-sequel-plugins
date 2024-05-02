f6045face0b9876547dc2dee25129af05d61cfc0
Yuri Smirnov <tycoooon@gmail.com>
2024-05-02 18:31:49 +0300

    fix rails db cmd (#38)

--- lib/umbrellio_sequel_plugins/rails_db_command.rb
+++ lib/umbrellio_sequel_plugins/rails_db_command.rb
@@ -9,45 +9,86 @@ class Rails::Command::DbconsoleCommand < Rails::Command::Base
   def perform
     require "rake"
     Rake.with_application(&:load_rakefile) # Needed to initialize Rails.application
-    Rails::DBConsole.start(options)
+    start!
   end
-end
-
-class Rails::DBConsole
-  DBConfig = Struct.new(:configuration_hash, :adapter, :database)
 
   private
 
-  def db_config
-    @db_config ||= DBConfig.new(configuration_hash, adapter, database)
-  end
+  # See ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.dbconsole
+  def start!
+    ENV["PGUSER"] = pg_config[:username] if pg_config[:username]
+    ENV["PGHOST"] = pg_config[:host] if pg_config[:host]
+    ENV["PGPORT"] = pg_config[:port].to_s if pg_config[:port]
 
-  def configuration_hash
-    return @configuration_hash if defined?(@configuration_hash)
-
-    rails_db_config = Rails.application.config.database_configuration
-
-    sequel_configuration = SequelRails::Configuration.new
-    SequelRails.configuration = sequel_configuration.merge!(raw: rails_db_config)
-
-    storage = SequelRails::Storage.adapter_for(Rails.env)
-    config = storage.config.with_indifferent_access
-
-    if @options[:server]
-      server_config = config.fetch(:servers).fetch(@options[:server])
-      config.merge!(server_config)
+    if pg_config[:password] && options[:include_password]
+      ENV["PGPASSWORD"] = pg_config[:password].to_s
     end
 
-    @configuration_hash = config
+    ENV["PGSSLMODE"] = pg_config[:sslmode].to_s if pg_config[:sslmode]
+    ENV["PGSSLCERT"] = pg_config[:sslcert].to_s if pg_config[:sslcert]
+    ENV["PGSSLKEY"] = pg_config[:sslkey].to_s if pg_config[:sslkey]
+    ENV["PGSSLROOTCERT"] = pg_config[:sslrootcert].to_s if pg_config[:sslrootcert]
+
+    if pg_config[:variables]
+      ENV["PGOPTIONS"] = pg_config[:variables].filter_map do |name, value|
+        "-c #{name}=#{value.to_s.gsub(/[ \\]/, '\\\\\0')}" unless value.in?([":default", :default])
+      end.join(" ")
+    end
+
+    find_cmd_and_exec("psql", database)
   end
 
-  def adapter
-    mapping = SequelRails::DbConfig::ADAPTER_MAPPING.invert
-    value = configuration_hash.fetch(:adapter)
-    mapping[value] || value
+  def pg_config
+    @pg_config ||= begin
+      rails_db_config = Rails.application.config.database_configuration
+
+      sequel_configuration = SequelRails::Configuration.new
+      SequelRails.configuration = sequel_configuration.merge!(raw: rails_db_config)
+
+      storage = SequelRails::Storage.adapter_for(Rails.env)
+      config = storage.config.with_indifferent_access
+
+      if @options[:server]
+        server_config = config.fetch(:servers).fetch(@options[:server])
+        config.merge!(server_config)
+      end
+
+      config
+    end
+  end
+
+  # See ActiveRecord::ConnectionAdapters::AbstractAdapter.find_cmd_and_exec
+  def find_cmd_and_exec(commands, *args) # rubocop:disable Metrics/MethodLength
+    commands = Array(commands)
+
+    dirs_on_path = ENV["PATH"].to_s.split(File::PATH_SEPARATOR)
+    unless (ext = RbConfig::CONFIG["EXEEXT"]).empty?
+      commands = commands.map { |cmd| "#{cmd}#{ext}" }
+    end
+
+    full_path_command = nil
+    found = commands.detect do |cmd|
+      dirs_on_path.detect do |path|
+        full_path_command = File.join(path, cmd)
+        begin
+          stat = File.stat(full_path_command)
+        rescue SystemCallError
+        else
+          stat.file? && stat.executable?
+        end
+      end
+    end
+
+    if found
+      exec(*[full_path_command, *args].compact)
+    else
+      abort(
+        "Couldn't find database client: #{commands.join(', ')}. Check your $PATH and try again.",
+      )
+    end
   end
 
   def database
-    @options[:database] || configuration_hash.fetch(:database)
+    options[:database] || pg_config.fetch(:database)
   end
 end
